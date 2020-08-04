871f10d443e37a2dee19263adda298fc2c8f9af8
dhurt <dhurt@DESKTOP-U6JE5R4>
2020-08-04 15:52:37 -0500

    08/04/2020 3:52

---
+++ DailyBlogger/DailyBlogger/Controllers/BlogController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DailyBlogger.Models;
+
+namespace DailyBlogger.Controllers
+{
+    public class BlogController : Controller
+    {
+        private BlogContext _context;
+        public BlogController(BlogContext context)
+        {
+            _context = context;
+        }
+        public IActionResult List()
+        {
+            IEnumerable<BlogPost> posts = _context.blogPost.ToList<BlogPost>();
+            return View();
+        }
+    }
+}
